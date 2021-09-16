7aec559c4a0763b6eae7dadd6b538ea0d29f32c5
Daksh Upadhyay <upadhyaydaksh96@gmail.com>
2021-09-16 17:26:47 -0400

    Change background AND text on button click.

--- DakshUpadhyayHelloWorld/ViewController.swift
+++ DakshUpadhyayHelloWorld/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var vwBg: UIView!
+    
+    @IBOutlet weak var lblTitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnChangeBg(_ sender: Any) {
+        self.changeBg()
+    }
+    
+    
+    func changeBg(){
+        self.vwBg.backgroundColor = .yellow
+        self.lblTitle.text = "Daksh Upadhyay \nHello World"
     }
 
 
